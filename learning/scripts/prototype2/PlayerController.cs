e6f07e9a851f4964450633975e601065e835cff8
Gavintain <74239444+Gavintain@users.noreply.github.com>
2024-03-15 20:16:02 +0900

    3.15

---
+++ learning/scripts/prototype2/PlayerController.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class PlayerController : MonoBehaviour
+{
+
+    private float input_xaxis;
+    private float input_spacebar;
+    private float offset_speed = 30f;
+    public GameObject projectilePrefab;
+    // Start is called before the first frame update
+    void Start()
+    { 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        input_xaxis = Input.GetAxis("Horizontal");
+        input_spacebar = Input.GetAxis("Jump");
+        // if ((transform.position.x < -10 && input_xaxis<0)||(transform.position.x > 10 && input_xaxis>0)){
+        // }
+        // else{
+        //     transform.Translate(offset_speed * Time.deltaTime * Vector3.right * input_xaxis);
+        // }
+        if (input_spacebar > 0.001):
+            
+        /// 플레이어의 X축 좌표가 경계값을 넘으면, 경계선으로 강제로 위치시키기.
+        if (transform.position.x < -10){
+            transform.position = Vector3.right * -10 + Vector3.up *  transform.position.y +  Vector3.forward *  transform.position.z;
+        }
+        else if  (transform.position.x > 10){
+            transform.position = Vector3.right * 10 + Vector3.up *  transform.position.y +  Vector3.forward *  transform.position.z;
+        }
+        else{
+            transform.Translate(offset_speed * Time.deltaTime * Vector3.right * input_xaxis);
+        }
+    }
+}
