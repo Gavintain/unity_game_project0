0efc7df3a69094db11a0e4a51c0d36b268dec891
Gavintain <74239444+Gavintain@users.noreply.github.com>
2024-03-20 22:45:58 +0900

    주니어 2단원 완료

---
+++ learning/scripts/prototype2/PlayerControllerX.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Timers;
+using UnityEngine;
+
+public class PlayerControllerX : MonoBehaviour
+{
+    public GameObject dogPrefab; 
+    public float reloadingTime = 2.5f;
+    private bool dogReload = true;
+    // Update is called once per frame
+    void Update()
+    {
+        // On spacebar press, send dog
+        if (Input.GetKeyDown(KeyCode.Space)&(dogReload))
+        {
+            Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
+            dogReload = false;
+            Invoke("DogReloading",reloadingTime);
+        }
+        
+    }
+    void DogReloading()
+    {
+        dogReload = true;
+    }
+}
